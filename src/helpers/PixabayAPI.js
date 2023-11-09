6db4762d293869bee366aaccbbe0777a7cd63195
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-11-09 12:59:05 +0200

    trend fetch

--- src/helpers/PixabayAPI.js
+++ src/helpers/PixabayAPI.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWZmYTlmNjVjYWUyNWIxZjU5N2IyNDhmZmZiZWQzMiIsInN1YiI6IjY1NGI2NjNhMWFjMjkyN2IyZGNmNzBiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wt2WcEuurlXNU4SFGIcSzLE3fb4Vg87kUc19yKbL2iA';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.headers.common['Authorization'] = API_KEY;
 
-export const getImages = (query, page) => {
-  const KEY = '39344710-8effa9f65cae25b1f597b248fffbed32';
-  const options = {
-    per_page: 12,
-    image_type: 'photo',
-    orientation: 'horizontal',
-  };
-  const searchParams = new URLSearchParams(options);
-  return axios.get(`?q=${query}&key=${KEY}&page=${page}&${searchParams}`);
+export const getMovies = time_window => {
+  return axios.get(`/trending/movie/${time_window}`);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getMovies } from 'helpers/PixabayAPI';
+
 export const Home = () => {
+  const [trendMovies, setTrendMovies] = useState([]);
+
+  useEffect(() => {
+    getTrendMovies();
+  }, []);
+
+  const getTrendMovies = async () => {
+    try {
+      const response = await getMovies('day');
+      setTrendMovies(response.data.results);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  console.log(trendMovies);
   return (
     <main>
       <h1>Welcome</h1>
-      <img src="https://via.placeholder.com/960x240" alt="" />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-        voluptatum voluptate.
-      </p>
+      <ul>
+        {trendMovies.length > 0 &&
+          trendMovies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <Link>
+                  <img
+                    src={`http://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    alt={movie.title}
+                    width="350"
+                  />
+                  <h3>{movie.original_title}</h3>
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
     </main>
   );
 };
