c6cbde4f559186e3f65980c1adffa000f39bd3ff
Maksym-Shcherbak <130525995+Maksym-Shcherbak@users.noreply.github.com>
2023-11-14 12:31:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Redirect, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
